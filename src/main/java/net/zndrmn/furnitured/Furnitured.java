75ca5cf549e137453fe0146c0a6f2eb06482ddd2
zndrmn <zanderman007.zm@gmail.com>
2022-03-15 23:07:35 -0500

    Refactored "Items" Code

--- src/main/java/net/zndrmn/furnitured/Furnitured.java
+++ src/main/java/net/zndrmn/furnitured/Furnitured.java
@@ -1,8 +1,6 @@
 package net.zndrmn.furnitured;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import net.zndrmn.furnitured.block.Blockz;
 import net.zndrmn.furnitured.item.Itemz;
 import net.zndrmn.furnitured.registry.Blocks;
@@ -20,12 +18,6 @@ public class Furnitured implements ModInitializer {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
-
-        // Remove \/ \/ \/
-        // After Refactoring
-        //
-        Itemz.registerItems();
-        Blockz.registerBlocks();
 
         Items.registerItems();
         Blocks.registerBlocks();
